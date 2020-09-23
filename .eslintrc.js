a2ba686001bc03e22caf910e26b3ba405e78e61c
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-09-23 00:42:49 -0500

    Base page created

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+const { disable } = require("debug");
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+			"no-tabs": "off",
+			"indent": "off"
+    }
+};
---
+++ main.js
@@ -0,0 +1,26 @@
+const windows = [];
+windows.push(document.getElementById('profile'));
+windows.push(document.getElementById('resume'));
+windows.push(document.getElementById('education'));
+windows.push(document.getElementById('projects'));
+windows.push(document.getElementById('portfolio'));
+
+const hideall = () => {
+	windows.forEach((item) => {
+		// eslint-disable-next-line no-param-reassign
+		item.style.display = 'none';
+	});
+};
+
+const show = (number) => {
+	hideall();
+	windows[number].style.display = 'block';
+};
+
+document.getElementById('icopic-profile').onclick = () => show(0);
+document.getElementById('icopic-experience').onclick = () => show(1);
+document.getElementById('icopic-education').onclick = () => show(2);
+document.getElementById('icopic-projects').onclick = () => show(3);
+document.getElementById('icopic-portofolio').onclick = () => show(4);
+
+document.getElementById('profile').style.display = 'block';
