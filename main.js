30ac84b0c83bb530edae73ec0fe88bfe3c70f305
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-09-23 01:28:34 -0500

    Improved icon selection

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 const windows = [];
+const icons = [];
 windows.push(document.getElementById('profile'));
 windows.push(document.getElementById('resume'));
 windows.push(document.getElementById('education'));
 windows.push(document.getElementById('projects'));
 windows.push(document.getElementById('portfolio'));
 
+icons.push(document.getElementById('profile-icon'));
+icons.push(document.getElementById('experience-icon'));
+icons.push(document.getElementById('education-icon'));
+icons.push(document.getElementById('projects-icon'));
+icons.push(document.getElementById('portofolio-icon'));
+
 const hideall = () => {
-	windows.forEach((item) => {
+	windows.forEach((item, index) => {
 		const element = item;
+		const border = icons[index];
 		element.style.display = 'none';
+		border.style.border = 'thin none white';
 	});
 };
 
 const show = (number) => {
 	hideall();
 	windows[number].style.display = 'block';
+	icons[number].style.border = 'thin solid white';
 };
 
 document.getElementById('icopic-profile').onclick = () => show(0);
